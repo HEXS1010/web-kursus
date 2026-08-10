914a624a73e08e8b2d5d11ce30db2bf88539d575
Komang Aswameda <aswameda18@gmail.com>
2026-08-10 16:13:49 +0800

    firts commit navbar

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      colors: {
+        body: '#FEEDBC',
+        primary: '#3b82f6',
+        secondary: '#f59e0b',
+        nav: '#FEEDBC'
+      },
+      fontFamily: {
+        heading: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+}
